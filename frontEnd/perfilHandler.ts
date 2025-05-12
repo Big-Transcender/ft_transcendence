5d41e6c3347b4fd30350801e2764b34fae6b1517
brennm2 <brendon_marcello@yahoo.com>
2025-05-12 18:57:19 +0100

    making main page

--- frontEnd/perfilHandler.ts
+++ frontEnd/perfilHandler.ts
@@ -1,44 +1,53 @@
-const API_URL = 'http://localhost:3001/usuarios'
+const API_URL = "http://127.0.0.1:3000/users";
+
 let users = [];
 
+fetch(API_URL)
+	.then((res) => res.json())
+	.then((data) => {
+		users = data;
+		data.forEach((user) => {
+			console.log(user.email);
+		});
 
-function updateWins(){
-	document.getElementById('winNumberId').innerText = getWins();
+		updateWins();
+		updateLoses();
+		updatePlays();
+		updateNick();
+	});
+
+function updateWins() {
+	document.getElementById("winNumberId").innerText = getWins();
 }
 
-function updateLoses(){
-	document.getElementById('loseNumberId').innerText = getLoses();
+function updateLoses() {
+	document.getElementById("loseNumberId").innerText = getLoses();
 }
 
-function updatePlays(){
-	document.getElementById('playNumberId').innerText = getPlays();
+function updatePlays() {
+	document.getElementById("playNumberId").innerText = getPlays();
 }
 
-function getWins(){
-	if (users[0] === undefined)
-		return "DEAD";
-	return users[0].wins;
+function updateNick() {
+	document.getElementById("profileNickId").innerText = getNick();
 }
 
-function getLoses(){
-	if (users[0] === undefined)
-		return "DEAD";
+function getNick() {
+	if (users[0] === undefined) return "DEAD";
+	return users[0].nickname;
+}
+
+function getWins() {
+	if (users[0] === undefined) return "DEAD";
+	return users[0].id;
+}
+
+function getLoses() {
+	if (users[0] === undefined) return "DEAD";
 	return users[0].loses;
 }
 
-function getPlays(){
-	if (users[0] === undefined)
-		return "DEAD";
+function getPlays() {
+	if (users[0] === undefined) return "DEAD";
 	return users[0].plays;
 }
-
-
-fetch(API_URL)
-	.then(res => res.json())
-	.then(data =>{
-		users = data;
-		data.forEach(user =>{
-			// console.log(user.id);
-		})
-	});
-
