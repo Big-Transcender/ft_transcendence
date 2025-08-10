bd02272271ac2e187483754f145b0bb4a3652dcf
Bruno Miguel <brunomiguel0798@gmail.com>
2025-08-10 13:29:58 +0100

    Continuing to improve the 2FA Routes

--- BackEnd/src/routes/2FactorAuth.js
+++ BackEnd/src/routes/2FactorAuth.js
@@ -19,15 +19,22 @@ module.exports = async function (fastify) {
 			.run(secret.base32, userId);
 
 		const qrCode = await qrcode.toDataURL(secret.otpauth_url);
-		return reply.send({ qr: qrCode);
+		return reply.send({ qr: qrCode});
 	});
 
 //route for verifying 2FA token
 	fastify.post('/2fa/verify', { preHandler: [fastify.authenticate]}, async (request, reply) => {
 		const { token } = request.body;
-		const user = request.session.get('user');
+		const userId = request.userId;
 
-		const row = await db.prepare('SELECT two_factor_secret FROM users WHERE id = ?').get(user.id);
+		if(!token)
+			return reply.code(400).send({ error: 'Token is required'});
+
+		const row = await db.prepare('SELECT two_factor_secret FROM users WHERE id = ?').get(userId);
+
+		if(!row || row.two_factor_secret)
+			return reply.code(400).send({ error: 'No 2FA setup not found'});
+
 
 		const verified = speakeasy.totp.verify({
 			secret: row.two_factor_secret,
@@ -38,26 +45,40 @@ module.exports = async function (fastify) {
 		if (!verified)
 			return reply.code(400).send({ error: 'Invalid token' });
 
-		await db.prepare('UPDATE users SET two_factor_enable = 1 WHERE id = ?').run(user.id);
-
+		await db.prepare('UPDATE users SET two_factor_enable = 1 WHERE id = ?').run(userId);
 		return reply.send({ success: true });
 	});
 
 	fastify.get('/2fa/status', { preHandler: [fastify.authenticate]},  async (request, reply) => {
-		const user = request.session.get('user');
-		if (!user)
-			return reply.code(401).send({ error: 'Not authenticated' });
+		const userId = request.userId;
 
-		const row = await db.prepare('SELECT two_factor_enable FROM users WHERE id = ?').get(user.id);
+		const row = await db.prepare('SELECT two_factor_enable FROM users WHERE id = ?').get(userId);
 		const enabled = row && row.two_factor_enable === 1;
 		return reply.send({ enabled });
 	});
 
 	fastify.post('/2fa/disable', { preHandler: [fastify.authenticate]}, async (request, reply) =>{
-		const user = request.session.get('user');
-		if(!user)
-			return reply.code(401).send({ error: 'Not authenticated' });
-		await db.prepare('UPDATE users SET two_factor_enable = 0, two_factor_secret = NULL where id = ?').run(user.id);
-		return reply.send({ success: true });
+		const { token } = request.body;
+		const userId = request.userId;
+
+		if(!token)
+			return reply.code(400).send({ error: 'Token is required'});
+
+		const row = db.prepare('SELECT two_factor_secret FROM users WHERE id = ?').get(userId);
+		if(!row || !row.two_factor_secret)
+			return reply.code(400).send({ error: '2FA not set up' });
+
+		const verified = speakeasy.totp.verify({
+			secret: row.two_factor_secret,
+			encoding: 'base32',
+			token
+		});
+
+		if(!verified)
+			return reply.code(400).send({ error: 'Invalid token '});
+
+		await db.prepare('UPDATE users SET two_factor_enable = 0, two_factor_secret = NULL WHERE id = ?').run(userId);
+
+		return reply.send({ success: true});
 	});
 };
