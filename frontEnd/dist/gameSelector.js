cf54d192aeba0b0f12b89c7d9d9e2fa94b5c38b4
Diogo Pinheiro <diogo.pinheiro1996@gmail.com>
2025-08-12 15:38:44 +0100

    merged with ponyta and worked

--- frontEnd/dist/gameSelector.js
+++ frontEnd/dist/gameSelector.js
@@ -151,30 +151,6 @@ function openPopupPong() {
     create2V2PopupButton.addEventListener("click", async () => {
         closePopupPong();
         openPopup2v2();
-        // 	const action = prompt("Type 'create' to start a new 2v2 match or 'join' to join one:");
-        // 	if (!action) return;
-        // 	if (action.toLowerCase() === "create") {
-        // 		createNewMatch(false, false, true); // teamGame = true
-        // 		closePopupPong();
-        // 	} else if (action.toLowerCase() === "join") {
-        // 		const matchId = prompt("Enter the 2v2 Match ID:");
-        // 		if (!matchId || !matchId.trim()) return;
-        // 		try {
-        // 			const matchData = await checkMatchExists(matchId.trim());
-        // 			if (matchData.exists) {
-        // 				joinExistingMatch(matchId.trim());
-        // 				showMatchId(matchId.trim());
-        // 				closePopupPong();
-        // 			} else {
-        // 				showErrorAndReturn("2v2 Match not found or is full.");
-        // 			}
-        // 		} catch (error) {
-        // 			const errorMessage = error instanceof Error ? error.message : "Unknown error";
-        // 			showErrorAndReturn(`Error checking match: ${errorMessage}`);
-        // 		}
-        // 	} else {
-        // 		alert("Invalid input. Type either 'create' or 'join'.");
-        // 	}
     });
     // JOIN A MP MATCH
     joinPopupButton.addEventListener("click", async () => {
@@ -191,13 +167,14 @@ function openPopupPong() {
                 closePopupPong();
             }
             else {
-                console.log("Match not found or is full.");
-                //displayWarning("Match not found or is full.");
+                //console.log("Match not found or is full.");
+                displayWarning("Match not found or is full.");
+                //showErrorAndReturn("Match not found or is full.");
             }
         }
         catch (error) {
             const errorMessage = error instanceof Error ? error.message : "Unknown error";
-            showErrorAndReturn(`Error checking match: ${errorMessage}`);
+            displayWarning(`Error checking match: ${errorMessage}`);
         }
     });
 }
@@ -234,7 +211,7 @@ function openPopup2v2() {
         }
         catch (error) {
             const errorMessage = error instanceof Error ? error.message : "Unknown error";
-            showErrorAndReturn(`Error checking match: ${errorMessage}`);
+            displayWarning(`Error checking match: ${errorMessage}`);
         }
     });
     // CREATE 2v2POPUP
